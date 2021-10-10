1acae8e9e738ce9d113134ba60cf08201256f585
trswork <87744214+trswork@users.noreply.github.com>
2021-10-09 23:17:00 -0400

    lib

---
+++ index.js
@@ -0,0 +1,7 @@
+// node modules 
+const fs = require('fs'); 
+const inquirer = require('inquirer');
+
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');

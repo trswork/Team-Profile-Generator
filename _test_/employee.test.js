d43fc781285da756099641fb18685c88026f6350
trswork <87744214+trswork@users.noreply.github.com>
2022-01-01 14:23:55 -0500

    update

---
+++ _test_/employee.test.js
@@ -0,0 +1,35 @@
+const Employee = require("./lib/employee");
+
+test("creates a employee object", () => {
+    const employee = new Employee();
+  
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.ID).toEqual(expect.any(String));
+    expect(employee.email).toEqual(expect.any(String));
+  });
+
+  test("gets employee name", () => {
+    const employee = new Employee();
+  
+    expect(employee.getName()).toEqual(expect.any(String));
+  });
+  
+  test("gets employee ID", () => {
+    const employee = new Employee();
+  
+    expect(employee.getID()).toEqual(expect.any(String));
+  });
+
+  test("gets employee Email", () => {
+    const employee = new Employee();
+  
+    expect(employee.getEmail()).toEqual(expect.any(String));
+  });
+  
+  test("gets employee Role", () => {
+    const employee = new Employee();
+  
+    expect(employee.getRole()).toEqual("Employee");
+  });
+  
+  
